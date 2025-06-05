595f4a83fcf66cbc2edaff8bb6e1c464667001bb
Ryan <r.mckee@qub.ac.uk>
2025-06-05 09:35:56 +0100

    adding default config for docker file for hybrid automaton

--- colav-hybrid-automaton/colav_hybrid_automaton/hybrid_automaton/scripts/nodes/hybrid_automaton_mission_control_node.py
+++ colav-hybrid-automaton/colav_hybrid_automaton/hybrid_automaton/scripts/nodes/hybrid_automaton_mission_control_node.py
@@ -30,6 +30,7 @@ from hybrid_automaton_interfaces.msg import DynamicParameter
 from colav_interfaces.msg import Waypoints
 
 SYSTEM_CLOCK = None
+DEFAULT_CONFIG_FILE_PATH = "/etc/colav_hybrid_automaton_config.yml"
 
 class HybridAutomatonMissionControlNode(Node):
     
@@ -95,7 +96,7 @@ class HybridAutomatonMissionControlNode(Node):
         if not self._automaton_params_setter_cli.wait_for_service(timeout_sec=30.0):
             self.get_logger().error('/colav/hybrid_automaton/lifecycle/set_parameters: srv not available!')
 
-        self._default_configuration_path = '/home/3507145@eeecs.qub.ac.uk/ros2_ws/src/colav-hybrid-automaton/colav-hybrid-automaton/colav_hybrid_automaton/config/colav_hybrid_automaton_config.yml'
+        self._default_configuration_path = DEFAULT_CONFIG_FILE_PATH
         self._default_evaluation_frequency = 10
         self._default_control_frequency = 100
         self._current_goal_waypoint:Waypoint = None
