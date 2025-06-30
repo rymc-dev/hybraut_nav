160e85b5ea185cc3306ca07c2f574ad0b33c8a72
Ryan McKee <r.mckee@qub.ac.uk>
2025-06-30 11:29:32 +0100

    added tests for status enums between different interfaces

--- colav-hybrid-automaton/colav_hybrid_automaton/colav_hybrid_automaton/automaton/_internal/constants/automaton_status.py
+++ colav-hybrid-automaton/colav_hybrid_automaton/colav_hybrid_automaton/automaton/_internal/constants/automaton_status.py
@@ -2,12 +2,12 @@ from enum import IntEnum
 from typing import Union
 
 class HybridAutomatonStatusEnum(IntEnum):
-    INITIALIZING = 1
-    ACTIVE_MODE = 2
-    TRANSITIONING = 3
-    COMPLETED = 4
-    DEACTIVATING = 5
-    ERROR = 6
+    INITIALIZING = 0
+    ACTIVE_MODE = 1
+    TRANSITIONING = 2
+    COMPLETED = 3
+    DEACTIVATING = 4
+    ERROR = 5
 
 # Use this for type hints
 HybridAutomatonStatusValue = Union[HybridAutomatonStatusEnum, int]
