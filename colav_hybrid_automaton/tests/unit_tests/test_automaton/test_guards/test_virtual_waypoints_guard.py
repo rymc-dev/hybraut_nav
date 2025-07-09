9e567281823c749b0e30c92ded0144102aacbb9b
Ryan <r.mckee@qub.ac.uk>
2025-07-09 14:12:16 +0100

    refactored virtual waypoints guard tests and added additional tests

--- colav_hybrid_automaton/tests/unit_tests/test_automaton/test_guards/test_virtual_waypoints_guard.py
+++ colav_hybrid_automaton/tests/unit_tests/test_automaton/test_guards/test_virtual_waypoints_guard.py
@@ -1,54 +1,75 @@
-from colav_hybrid_automaton.automaton.guards import VirtualWaypointsGuard
+from colav_hybrid_automaton.automaton.guards import VirtualWaypointsGuard, GuardABC
 import pytest
 from colav_interfaces.msg import WaypointsState as ROSWaypointsState, Waypoint as ROSWaypoint
 
+@pytest.fixture
+def guard_instance():
+    return VirtualWaypointsGuard()
 
-@pytest.mark.parametrize(
-    "waypoints_state, expected_guard_evaluation, test_description",
-    [
-        # If there is at least one virtual waypoint → guard should fire (True)
-        (ROSWaypointsState(virtual_waypoints=[ROSWaypoint()]), True, "One virtual waypoint → True"),
+class TestVirtualWaypointsGuard:
+    """test suite for virutal waypoints guard"""
 
-        # If there are no virtual waypoints → guard should not fire (False)
-        (ROSWaypointsState(virtual_waypoints=[]), False, "Empty virtual_waypoints list → False"),
+    def test_initialization_and_specs(self, guard_instance: GuardABC):
+        """test the initialization and specifications of the class instance are valid"""
+        assert guard_instance.init_input_spec_names() == []
+        assert guard_instance.init_input_spec_types() == []
+        assert guard_instance.state_input_spec_names() == ['waypoints_state']
+        assert guard_instance.state_input_spec_types() == [ROSWaypointsState] 
 
-        # If the field is omitted (defaults to empty) → guard should not fire
-        (ROSWaypointsState(), False, "No virtual_waypoints attribute set → False"),
-    ]
-)
-def test_virtual_waypoints_guard_comprehensive(
-    waypoints_state: ROSWaypointsState,
-    expected_guard_evaluation: bool,
-    test_description: str
-):
-    """
-    Comprehensive tests for VirtualWaypointsGuard:
-    - fires when there is at least one virtual waypoint
-    - does not fire when there are none
-    """
-    guard = VirtualWaypointsGuard()
+    def test_string_representations(self, guard_instance: GuardABC):
+        """test the string representations of the guard instance"""
+        assert repr(guard_instance) == 'VirtualWaypointsGuard(initialized=True)'
+        assert str(guard_instance) == 'Guard Function: VirtualWaypointsGuard'
 
-    # Call with only the waypoints_state
-    actual = guard.__call__(waypoints_state=waypoints_state)
+    @pytest.mark.parametrize(
+        "waypoints_state, expected_guard_evaluation",
+        [
+            (ROSWaypointsState(virtual_waypoints=[ROSWaypoint()]), True),
+            (ROSWaypointsState(virtual_waypoints=[]), False),
+            (ROSWaypointsState(), False),
+        ],
+        ids=[
+            "One virtual waypoint → True",
+            "Empty virtual_waypoints list → False",
+            "No virtual_waypoints attribute set → False"
+        ]
+    )
+    def test_guard_evaluation(
+        self,
+        waypoints_state: ROSWaypointsState,
+        expected_guard_evaluation: bool,
+        request,
+        guard_instance
+    ):
+        """
+        Comprehensive tests for VirtualWaypointsGuard:
+        - fires when there is at least one virtual waypoint
+        - does not fire when there are none
+        """
+        assert guard_instance(waypoints_state=waypoints_state) == expected_guard_evaluation, request.node.callspec.id
 
-    assert actual == expected_guard_evaluation, test_description
+    # NOTE: No initialization args for this guard so no init_kwarg validation required
 
-@pytest.mark.parametrize(
-    "state_kwargs, expected_exception, test_description",
-    [
-        # No state args passed for __call__ ()
-        ({}, KeyError, "No args -> pytest.error(ValueError)"),
+    @pytest.mark.parametrize(
+        "state_kwargs, expected_exception",
+        [
+            ({}, KeyError),
+            ({'waypoints_state': "invalid_type"}, TypeError),
+        ],
+        ids=[
+            "No args -> pytest.error(ValueError)",
+            "Invalid State -> pytest.error(TypeError)"
+        ]       
+    )
+    def test_invalid_state_kwargs(
+        self,
+        state_kwargs: ROSWaypointsState,
+        expected_exception: Exception,
+        guard_instance
+    ):
+        """test guard call with invalid args"""
+        with pytest.raises(expected_exception):
+            guard_instance.__call__(**state_kwargs)
 
-        # If there are no virtual waypoints → guard should not fire (False)
-        ({'waypoints_state': "invalid_type"}, TypeError, "Invalid State -> pytest.error(TypeError)"),
-    ]
-)
-def test_virtual_waypoints_guard_invalid_state_kwargs(
-    state_kwargs: ROSWaypointsState,
-    expected_exception: Exception,
-    test_description: str
-):
-    guard = VirtualWaypointsGuard()
-
-    with pytest.raises(expected_exception):
-        guard.__call__(**state_kwargs)
+if __name__ == '__main__':
+    pytest.main([__file__])
