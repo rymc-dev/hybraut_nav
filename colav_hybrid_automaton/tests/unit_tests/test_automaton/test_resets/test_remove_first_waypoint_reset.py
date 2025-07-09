553c093c4ed3fd635cc1d0d16b574444004ab68e
Ryan <r.mckee@qub.ac.uk>
2025-07-09 14:42:15 +0100

    updates

--- colav_hybrid_automaton/tests/unit_tests/test_automaton/test_resets/test_remove_first_waypoint_reset.py
+++ colav_hybrid_automaton/tests/unit_tests/test_automaton/test_resets/test_remove_first_waypoint_reset.py
@@ -14,41 +14,42 @@ from colav_interfaces.msg import WaypointsState as ROSWaypointsState, Waypoint a
 from geometry_msgs.msg import Point
 from colav_hybrid_automaton.automaton.resets import RemoveVirtualWaypointReset, ResetABC # Replace with actual import path
 
+@pytest.fixture
+def reset_instance():
+    return RemoveVirtualWaypointReset()
 
-@pytest.mark.parametrize(
-    "state_kwargs, vw_length_post_reset",
-    [
-        # Test 1: Remove a virtual waypoint, vws go from 1 to 0
-        (
-            {
-                "waypoints_state": ROSWaypointsState(
-                    virtual_waypoints=[
-                        ROSWaypoint(position=Point(x=1.0, y=2.0, z=0.0), acceptance_radius=10.0)
-                    ]
-                )
-            },
-            0
-        ),
-        # Test 2: Remove a virtual waypoint, vws go from 2 to 1
-        (
-            {
-                "waypoints_state": ROSWaypointsState(
-                    virtual_waypoints=[
-                        ROSWaypoint(position=Point(x=1.0, y=2.0, z=0.0), acceptance_radius=10.0),
-                        ROSWaypoint(position=Point(x=20.0, y=10.0, z=0.0), acceptance_radius=10.0)
-                    ]
-                )
-            },
-            1
-        )
-    ],
-    ids=["Test 1: Remove virtual waypoint with specific position", "Test 2: Remove virtual waypoint with duplicate position"]
-)
-def test_remove_virtual_waypoint_reset_comprehensive(state_kwargs: dict, vw_length_post_reset: dict):
-    reset: ResetABC = RemoveVirtualWaypointReset()
+class TestRemoveVirtualWaypointReset:
+    @pytest.mark.parametrize(
+        "state_kwargs, vw_length_post_reset",
+        [
+            (
+                {
+                    "waypoints_state": ROSWaypointsState(
+                        virtual_waypoints=[
+                            ROSWaypoint(position=Point(x=1.0, y=2.0, z=0.0), acceptance_radius=10.0)
+                        ]
+                    )
+                },
+                0
+            ),
+            (
+                {
+                    "waypoints_state": ROSWaypointsState(
+                        virtual_waypoints=[
+                            ROSWaypoint(position=Point(x=1.0, y=2.0, z=0.0), acceptance_radius=10.0),
+                            ROSWaypoint(position=Point(x=20.0, y=10.0, z=0.0), acceptance_radius=10.0)
+                        ]
+                    )
+                },
+                1
+            )
+        ],
+        ids=["Test 1: Remove virtual waypoint with specific position", "Test 2: Remove virtual waypoint with duplicate position"]
+    )
+    def test_reset(self, state_kwargs: dict, vw_length_post_reset: dict, reset_instance):
+        """test reset ouptput"""
+        reset_outputs = reset_instance(**state_kwargs)
 
-    reset_outputs = reset(**state_kwargs)
-
-    reset_output: ROSWaypointsState = reset_outputs['waypoints_state']
-    vws = reset_output.virtual_waypoints
-    assert len(vws) == vw_length_post_reset, "Reset output does not match expected result"
+        reset_output: ROSWaypointsState = reset_outputs['waypoints_state']
+        vws = reset_output.virtual_waypoints
+        assert len(vws) == vw_length_post_reset, "Reset output does not match expected result"
