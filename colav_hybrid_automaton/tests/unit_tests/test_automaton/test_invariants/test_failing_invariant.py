21fa04f6d67e8caa02b0285a4d38ae152e69afe5
Ryan <r.mckee@qub.ac.uk>
2025-07-09 14:39:10 +0100

    updates to trivial and failing invariant tests

--- colav_hybrid_automaton/tests/unit_tests/test_automaton/test_invariants/test_failing_invariant.py
+++ colav_hybrid_automaton/tests/unit_tests/test_automaton/test_invariants/test_failing_invariant.py
@@ -1,6 +1,28 @@
-from colav_hybrid_automaton.automaton.invariants import FailingInvariant
+import pytest
+from colav_hybrid_automaton.automaton.invariants import FailingInvariant, InvariantABC
 
-def test_failing_invariant_comprehensive():
-    """Comprehensive test for FailingInvariant."""
-    invariant = FailingInvariant()
-    assert invariant.__call__() == False, 'Failing invariant call should be false, but got true'
+@pytest.fixture
+def failing_invariant_instance():
+    return FailingInvariant()
+
+class TestFailingInvariant:
+    """test """
+    def test_initialization_and_specs(self, failing_invariant_instance: InvariantABC):
+        """test the initialization and specifications of the class instance are valid"""
+        assert failing_invariant_instance.init_input_spec_names() == []
+        assert failing_invariant_instance.init_input_spec_types() == []
+        assert failing_invariant_instance.state_input_spec_names() == []
+        assert failing_invariant_instance.state_input_spec_types() == [] 
+
+    def test_string_representations(self, failing_invariant_instance: InvariantABC):
+        """test the string representations of the guard instance"""
+        assert repr(failing_invariant_instance) == 'FailingInvariant(initialized=True)'
+        assert str(failing_invariant_instance) == 'Invariant Function: FailingInvariant'
+
+    def test_invariant_evaluation(self, failing_invariant_instance):
+        """Comprehensive test for FailingInvariant."""
+        assert failing_invariant_instance() == False, 'Failing invariant call should be false, but got true'
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
--- colav_hybrid_automaton/tests/unit_tests/test_automaton/test_invariants/test_trivial_invariant.py
+++ colav_hybrid_automaton/tests/unit_tests/test_automaton/test_invariants/test_trivial_invariant.py
@@ -1,6 +1,28 @@
-from colav_hybrid_automaton.automaton.invariants import TrivialInvariant
+import pytest
+from colav_hybrid_automaton.automaton.invariants import TrivialInvariant, InvariantABC
 
-def test_failing_invariant_comprehensive():
-    """Comprehensive test for FailingInvariant."""
-    invariant = TrivialInvariant()
-    assert invariant.__call__() == True, 'Trivial invariant call should be True, but got False'
+@pytest.fixture
+def trivial_invariant_instance():
+    return TrivialInvariant()
+
+class TestTrivialInvariant:
+    """test """
+    def test_initialization_and_specs(self, trivial_invariant_instance: InvariantABC):
+        """test the initialization and specifications of the class instance are valid"""
+        assert trivial_invariant_instance.init_input_spec_names() == []
+        assert trivial_invariant_instance.init_input_spec_types() == []
+        assert trivial_invariant_instance.state_input_spec_names() == []
+        assert trivial_invariant_instance.state_input_spec_types() == [] 
+
+    def test_string_representations(self, trivial_invariant_instance: InvariantABC):
+        """test the string representations of the guard instance"""
+        assert repr(trivial_invariant_instance) == 'TrivialInvariant(initialized=True)'
+        assert str(trivial_invariant_instance) == 'Invariant Function: TrivialInvariant'
+
+    def test_invariant_evaluation(self, trivial_invariant_instance):
+        """Comprehensive test for FailingInvariant."""
+        assert trivial_invariant_instance() == True, 'Trivial invariant call should be true, but got false'
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
