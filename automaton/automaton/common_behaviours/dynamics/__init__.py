c876eb7627406b88a1ae5edda7e01a4ed2c0902a
Ryan McKee <r.mckee@qub.ac.uk>
2025-07-17 22:07:15 +0100

    added some sample controllers to common_behaviours dynamics

---
+++ automaton/automaton/common_behaviours/dynamics/__init__.py
@@ -0,0 +1,11 @@
+from .mpc_controller import ModelPredictiveController
+from .pid_controller import PIDControllerDynamics
+from .pure_pursuit import PurePursuitController
+from .stanley_controller import StanleyController
+
+__all__ = [
+    'ModelPredictiveController',
+    'PIDControllerDynamics',
+    'PurePursuitController',
+    'StanleyController'
+]
