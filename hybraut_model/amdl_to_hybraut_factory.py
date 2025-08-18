c5740a9e842ccef7e14c2cceab1b4bc7c78b5167
Ryan McKee <r.mckee@qub.ac.uk>
2025-08-18 12:49:04 +0100

    moved factory functions out of dynamics

--- hybraut_model/amdl_to_hybraut_factory.py
+++ hybraut_model/amdl_to_hybraut_factory.py
@@ -6,8 +6,79 @@ from hybraut_model.automaton_types import MsgType
 
 logger = logging.getLogger(__name__)
 
+from hybraut_model._dynamics import DynamicsWrapper, DynamicsRegistry
+from hybraut_model.automaton_types import ComponentPath
 
-class StateFactory:
+
+class _DynamicsFactory:
+
+    component_cls = DynamicsWrapper
+
+    @classmethod
+    def load_dynamics_from_amdl(
+        cls, dynamics_name: str, dynamics_dict: Dict[str, Any]
+    ) -> DynamicsWrapper:
+        component_path = ComponentPath.load_component_from_famd(dynamics_dict)
+        component_class = component_path.get_component_class()
+        configuration = dynamics_dict.get("configuration")
+        output = dynamics_dict.get("output")
+        output_topic = output.get("topic")
+        msg_type_info = MsgType(**output["type"])
+        msg_type = msg_type_info.import_msg_type()
+
+        return cls(
+            _name=dynamics_name,
+            _component_class=component_class,
+            _configuration=configuration,
+            _output_topic=output_topic,
+            _output_msg_type=msg_type,
+        )
+
+    @classmethod
+    def register(cls, dynamics_dict: Dict[str, Any]) -> Dict[str, DynamicsWrapper]:
+        components = {}
+        for name, conf in dynamics_dict.items():
+            try:
+                component_cls = cls._component_class()
+                component = component_cls.load_dynamics_from_amdl(
+                    dynamics_name=name, dynamics_dict=conf
+                )
+                components[name] = component
+            except Exception as e:
+                logging.getLogger(__name__).error(
+                    f"Failed to import component '{name}': {e}"
+                )
+
+        return components
+
+    @classmethod
+    def load_dynamics_registry_from_amdl(
+        cls, dynamics_dict: Dict[str, Any]
+    ) -> DynamicsRegistry:
+        """generates the dyanmics registry from amdl"""
+        logger.info("Loading DynamicsRegistry from 'amdl' configuration")
+        dynamics = cls.register(dynamics_dict)
+        registry = cls(_components=dynamics)
+        logger.info(f"Created DynamicsRegistry with {len(dynamics)} dynamics")
+        return registry
+
+
+class _InvariantFactory: ...
+
+
+class _GuardFactory: ...
+
+
+class _ResetFactory: ...
+
+
+class _TransitionFactory: ...
+
+
+class _ModeFactory: ...
+
+
+class _StateFactory:
     component_cls = State
     logger = logging.getLogger(__name__)
 
@@ -111,3 +182,6 @@ class StateFactory:
         registry = cls(_components=states)
         cls.logger.info(f"Created StateRegistry with {len(states)} states")
         return registry
+
+
+class HybridAutomatonFactory: ...
