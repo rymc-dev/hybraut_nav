3180e3ca408342fc0628c772d0e0610a483f77ff
Ryan McKee <r.mckee@qub.ac.uk>
2025-10-17 04:41:07 +0100

    updates

--- hybraut_nav/launch/launch_strategic_node.py
+++ hybraut_nav/launch/launch_strategic_node.py
@@ -1,5 +1,23 @@
+import os
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+from hybraut_nav_strategy import StrategyNode
+
 def main():
-    pass
+    rclpy.init()
+
+    node = StrategyNode()
+    executor = MultiThreadedExecutor(num_threads=os.cpu_count())
+
+    try:
+        executor.spin()
+    except Exception as e: 
+        raise e
+    except KeyboardInterrupt: 
+        print (f'Keyboard interrupt.')
+
+
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
